4c4b26c31ef2e870223309e9e3252411c5120534
wildanfh <wildanf2005@gmail.com>
2026-03-10 23:26:33 +0100

    coverage report and challenge

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -108,4 +108,32 @@ contract RaffleTest is Test {
         //Assert
         assert(!upkeepNeeded);
     }
+
+    function testCheckUpkeepReturnsFalseIfEnoughTimeHasntPassed() public {
+        // Arrange
+        vm.deal(PLAYER, STARTING_PLAYER_BALANCE);
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+
+        // Act
+        (bool upkeepNeeded,) = raffle.checkUpkeep("");
+
+        // Assert
+        assert(!upkeepNeeded);
+    }
+
+    function testCheckUpkeepReturnsTrueWhenParametersGood() public {
+        // Arrange
+        vm.deal(PLAYER, STARTING_PLAYER_BALANCE);
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+
+        // Act
+        (bool upkeepNeeded,) = raffle.checkUpkeep("");
+
+        // Assert
+        assert(upkeepNeeded);
+    }
 }
